74a57fe11f8e11acfcf1fae4c8b837be64c4748e
Sean Anderson <Sean Anderson>
2017-03-19 23:34:10 +1300

    pass tournament variable to view

--- app/Http/Controllers/TournamentsController.php
+++ app/Http/Controllers/TournamentsController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Tournament;
 
 class TournamentsController extends Controller
 {
     //
 
-    public function show()
+    public function show(Tournament $tournament)
     {
-        return view('tournaments.show');
+        return view('tournaments.show')->with(compact('tournament'));
     }
 }
