0f31f77f9297eec3ed4af7dacae915d2a815fb2d
Sean Anderson <Sean Anderson>
2017-03-19 22:53:35 +1300

    Add Tournament Model

--- tests/Feature/ViewTournamentListingTest.php
+++ tests/Feature/ViewTournamentListingTest.php
@@ -7,6 +7,9 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
+use App\Tournament;
+use Carbon\Carbon;
+
 class ViewTournamentListingTest extends TestCase
 {
 
