a289bdabfe90f7a7393c3b08331726b13525dc82
Sean Anderson <Sean Anderson>
2017-03-19 22:52:34 +1300

    Add ViewTournamentListingTest

---
+++ tests/Feature/ViewTournamentListingTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ViewTournamentListingTest extends TestCase
+{
+
+    /** @test */
+    function user_can_view_a_tournament_listing()
+    {
+        //Arrange
+
+        $tournament = Tournament::create([
+            'title' => 'Tournie 2017',
+            'description' => 'A Sample tournament',
+            'venue' => 'Sample Venue',
+            'starts_at' => Carbon::parse('14 March, 2017'),
+            'ends_at' =>  Carbon::parse('16 March, 2017'),
+        ]);
+
+        // Act
+        $this->visit('/tournaments/' . $tournament->id);
+
+
+        // Assert
+        $this->see('Tournie 2017');
+        $this->see('A Sample tournament');
+        $this->see('Sample Venue');
+    }
+}
